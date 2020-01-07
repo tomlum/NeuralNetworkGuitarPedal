2cb3b691b9a47348936b0f2b6d7848a8dcd5e435
tomlum <checkeredlines@gmail.com>
2020-01-07 01:28:15 -0500

    init

---
+++ run.py
@@ -0,0 +1,43 @@
+# INSTALL > pipenv install
+# RUN     > pipenv run python run.py NAME_OF_SAVE_FILE
+import argparse
+import numpy as np
+import sounddevice as sd
+from keras.models import load_model
+from keras.backend import clear_session
+
+# Setup CLI arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('save_file',
+                    help="save file to use")
+args = parser.parse_args()
+
+# Arguments
+buffer_size = 512
+
+clear_session()
+# Load the model
+model = load_model(args.save_file)
+model._make_predict_function()
+
+live_array = np.zeros(buffer_size).reshape(1, buffer_size)
+
+def predict(in_data, out_data, frames, t, status):
+    global model, live_array
+
+    current_data = np.array([in_data.reshape(buffer_size)])
+
+    result_data = model.predict(current_data)
+    result_data = result_data.flatten()
+    result_data = result_data.reshape(buffer_size, 1)
+    out_data[:] = result_data
+
+with sd.Stream(device=(None, None),
+               samplerate=44100, blocksize=buffer_size,
+               dtype=None, latency=None,
+               channels=1, callback=predict):
+    print('~' * 80)
+    print("~~~DEEP WAVE BEND ACTIVE~~~")
+    print('---press Return to quit---')
+    print('~' * 80)
+    input()
