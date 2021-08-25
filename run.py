39c0c7ff22e725bf645235c97a4d1d4bdfbc0583
tomlum <tomlumperson@gmail.com>
2021-08-25 12:20:19 -0400

    removes hum

--- run.py
+++ run.py
@@ -26,11 +26,14 @@ def predict(in_data, out_data, frames, t, status):
     global model, live_array
 
     current_data = np.array([in_data.reshape(buffer_size)])
-
     result_data = model.predict(current_data)
     result_data = result_data.flatten()
     result_data = result_data.reshape(buffer_size, 1)
-    out_data[:] = result_data
+    maxx = np.amax(result_data)
+    if(maxx > 0.022):
+        out_data[:] = result_data
+    else:
+        out_data[:] = np.zeros(buffer_size).reshape(buffer_size, 1)
 
 with sd.Stream(device=(None, None),
                samplerate=44100, blocksize=buffer_size,
